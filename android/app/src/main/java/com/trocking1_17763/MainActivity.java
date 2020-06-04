b02098802e0413fe4c625e1bca131c2f12f388f7
Crowdbotics <team@crowdbotics.com>
2020-06-04 00:25:54 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/trocking1_17763/MainActivity.java
@@ -0,0 +1,15 @@
+package com.trocking1_17763;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "trocking1_17763";
+    }
+}
